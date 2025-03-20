7a272a3eab6a18e8000a2ede0fcc5dd0f3e44693
Marcelo Ebert <ebert.marcelo@gmail.com>
2025-03-20 16:29:32 -0300

    update of module project to framesnap-login

--- src/main/java/com/fiap/framesnap/crosscutting/config/DynamoDBConfig.java
+++ src/main/java/com/fiap/framesnap/crosscutting/config/DynamoDBConfig.java
@@ -20,9 +20,9 @@ public class DynamoDBConfig {
         String sessionToken = System.getenv("AWS_SESSION_TOKEN");*/
 
         String region = "us-east-1";
-        String accessKey = "ASIAR3F4HGIBNSKFBY3V";
-        String secretKey = "+KeoYabTL/1Xgq+D5QhmuyDSMIFb4dJGXZ2l9jN1";
-        String sessionToken = "IQoJb3JpZ2luX2VjEEIaCXVzLXdlc3QtMiJGMEQCICurtKdS66wlJTta9OJ0e7PhuLBpWvpSqa6CM9NuKBNUAiB3YrfuTB5DeLuxTKBnmguDX10Npfr7ZxHINhLuOX5m1iqxAghrEAAaDDEyNzA5NjI3MTM2MiIMITWHI0f2/BS9hjaMKo4C6yStEcMbHlWw2aN4byfiu4UMvmYe7QWbRN3nwtTEafi6d1gYmU1N2nn/xi+4FJWrUY8BfhD/kNV/dSS5yk0BN/X/IFnLctRqa2+OPaqsAJxM5biF3v7zUETB+QaNsuFHBdNKf+zwQBamjyIjbi4QS8bpQn3xqWd4riIUfs6e1ebFePXIFKJvbnHUOmGWIQ6MiwWsi0gka+tFmoFgv+PbC034d+0mzuOqEUM9u/7Q/BvnowFmLRx/co/5Aem16Lqj8+Szd/AEPeH5bww0ivd4ywiPneUtX+UgfzUWizkm0v247KoUK7+/QVVmpC7qn9W91Cwx6b75sITpwIVGuthE4VSB6J7ruLtXHtEhZZ5pMKCnyr0GOp4BHPz0MAYXwPcndlfigNDZYvjA7/fHlScU2WYXOShLdIKCatPjYOaLVzHKzvBvfMtfwvFnGvrL0xTNpli6EouZGA/SKYzLbH/wNd4KI7jQTlYVN3q/dO9Rsp/p7b8rlnGPmv5WK2nWyFBMk6OcWyr5sVq88rP3gZMdlnsRYDfeTIXT/aSiIjeEpwO8/4RIqcG8ek9oShX+HLauKnsM1rk=";
+        String accessKey = "ASIA2WJTGM7JQ3MYHQ5K";
+        String secretKey = "C9JlgzIPvpP6HOQgAcAV331yRSebaBOezjnysm4/";
+        String sessionToken = "IQoJb3JpZ2luX2VjEDsaCXVzLXdlc3QtMiJHMEUCIQCcHV6V3H0ZepvKwA04qGI1bFF9akM87uDQdMq3I9zFMgIgV4ENyMk//veVNXAJB2gR/2xA5GvRzIwnQz0kikMUh4oqugIIlP//////////ARAAGgw3MzUwODM2NTMwNzUiDIm1qs+J9HJffH1GeSqOAt/3zg7lP1eYYRi6YTVUAsigENsd4h3B1kIF3vO1qhAHdq3GyoPi9PyXchUKXRk2jN/sbZkP1R55LPUEjvZDshZKXiyAi6eq/riAGiNJ7uBAwxC22cxN4/Es+auF5dy8Gbvi97IPv384ODUcEz0G1mpIHN82H060Xx5twgphoqaZciQQtDZCTbEQ6BaQv17z3jnC6QLoXRB7zEa1dCeLoF96F+ETKse3DmJwV1sXxlzHVrLrCzzoUn55n1s0/1GIIIusVqigp2zZ1t0kzX9GLOrHu112JDVVM6FkSALSv94UYMKCY3TtzaoztBtUnzATXrsIkW67qID1bcyjVeuiUUS7owm4++8WsTqBuqFPRzCKvPG+BjqdAc7ILv4hLKm8xtr9pYo+DNopzEx7s0T+3yBlqJsYEsn3GN5jjNfRjOt2RLJGH3sts3PoR5TRU8JpXL2hTR9i8T+hpZdRwff1sAe7WYutvjjm+maMUEclb/yfFfIhBvptOVGEhMXd01ZmU60B0sRqvaQRpjySmLYFrkcL1wIJFI3trbL2mFPPQxse77eklmsU1wqfO9SM0hoPltwiymw=";
 
         return DynamoDbClient.builder()
                 .region(Region.of(region))
