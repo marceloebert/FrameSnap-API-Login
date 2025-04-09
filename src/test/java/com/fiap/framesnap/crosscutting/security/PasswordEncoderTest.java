d5af3617cf3e2d304f2e1847d7da2b67acf822f1
Marcelo Ebert <ebert.marcelo@gmail.com>
2025-04-08 23:14:41 -0300

    Cognito properties

--- src/test/java/com/fiap/framesnap/crosscutting/security/PasswordEncoderTest.java
+++ src/test/java/com/fiap/framesnap/crosscutting/security/PasswordEncoderTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class PasswordEncoderTest {
+class PasswordEncoderTest2 {
 
     private PasswordEncoder passwordEncoder;
 
